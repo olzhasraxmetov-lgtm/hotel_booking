3cd0a9f62c7076eeba8056a698f491f79d7d2be6
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-02-01 15:52:41 +0500

    add patch and put endpoints

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import uvicorn
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Body
 
 app = FastAPI()
 
 hotels = [
-    {"id": 1, "name": "Sochi"},
-    {"id": 2, "name": "Dubai"},
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Dubai", "name": "dubai"},
 ]
 @app.get("/hotels")
 async def get_hotels(
         id: int | None = Query(None, description="Hotel ID"),
-        name: str | None = Query(None, description="Hotel Name"),
+        title: str | None = Query(None, description="Hotel Name"),
 ):
     hotels_ = []
     for hotel in hotels:
         if id and hotel["id"] != id:
             continue
-        if name and hotel["title"] != name:
+        if title and hotel["title"] != title:
             continue
         hotels_.append(hotel)
     return hotels_
@@ -29,8 +29,45 @@ async def delete_hotel(
     hotels = [hotel for hotel in hotels if hotel['id'] != hotel_id]
     return {"success": "ok:"}
 
+@app.post("/hotels")
+async def create_hotel(
+        title: str = Body(..., embed=True),
+):
+    global hotels
+    data = {
+        "id": hotels[-1]["id"] + 1,
+        "title": title,
+    }
+    hotels.append(data)
+    return {"success": "ok:"}
 
+@app.put("/hotels/{hotel_id}")
+async def update_hotel(
+        hotel_id: int,
+        title: str = Body(...),
+        name: str = Body(...),
+):
+    global hotels
+    for hotel in hotels:
+        if hotel['id'] == hotel_id:
+            hotel["title"] = title
+            hotel["name"] = name
+    return {"success": "ok:"}
 
+@app.patch("/hotels/{hotel_id}")
+async def update_hotel_partially(
+        hotel_id: int,
+        title: str | None = Body(default=None),
+        name: str | None = Body(default=None),
+):
+    global hotels
+    for hotel in hotels:
+        if hotel['id'] == hotel_id:
+            if title:
+                hotel["title"] = title
+            elif name:
+                hotel["name"] = name
+    return {"success": "ok:"}
 
 
 if __name__ == "__main__":
