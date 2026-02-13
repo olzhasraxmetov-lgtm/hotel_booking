d7bdbf37eeeaf0827ad28e43f0ddea47e9c118a4
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-02-13 16:03:42 +0500

    add BaseRepository

---
+++ src/repositories/base.py
@@ -0,0 +1,20 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.models.hotels import HotelsORM
+
+class BaseRepository:
+    model = None
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+
+    async def get_all(self):
+        query = select(self.model)
+        result = await self.session.execute(query)
+        return result.scalars().all()
+
+    async def get_one_or_none(self, **filter_by):
+        query = select(self.model).filter_by(**filter_by)
+        result = await self.session.execute(query)
+        return result.scalars().one_or_none()
