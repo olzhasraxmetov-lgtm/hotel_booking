2974bd28e1fc14672ecae12cbe14f4de64692562
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-02-16 17:17:44 +0500

    add AuthService to handle auth logic and simplify endpoints

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,14 +1,15 @@
-from fastapi import APIRouter, Body
-from passlib.context import CryptContext
+
+
+from fastapi import APIRouter, Body, HTTPException, Response
 
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserCreate
 from src.schemas.users import UserRequestCreate
+from src.services.auth import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @router.post("/register")
 async def login(
@@ -19,7 +20,7 @@ async def login(
             }},
         })
 ):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService.hash_password(data.password)
     new_user_data = UserCreate(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         await UsersRepository(session).add(new_user_data)
@@ -30,9 +31,18 @@ async def login(
 
 
 @router.post("/login")
-async def login():
-    pass
+async def login_user(
+        data: UserRequestCreate,
+        response: Response
+):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail='Пользователь с таким email не существует')
 
-@router.post("/logout")
-async def login():
-    pass
+        if not AuthService().verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail='Пароль неверный')
+
+        access_token = AuthService().create_access_token(data={"sub": user.id})
+        response.set_cookie("access_token", access_token)
+        return {"access_token": access_token}
