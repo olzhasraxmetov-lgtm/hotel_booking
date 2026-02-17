f998c0b1e2b562b28a59c765d0f70e47816071a5
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-02-17 17:32:57 +0500

    feat(notification): implement NotificationService with redis

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 
 import sys
 from pathlib import Path
+
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as hotels_router
@@ -11,7 +12,6 @@ from src.api.auth import router as auth_router
 app = FastAPI()
 app.include_router(auth_router)
 app.include_router(hotels_router)
-
 
 if __name__ == "__main__":
     uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
