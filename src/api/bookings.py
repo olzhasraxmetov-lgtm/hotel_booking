ef9af9a8629d2cdbcfd9b4ab60702c00d75c92f3
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-02-18 18:02:14 +0500

    add /bookings and /bookings/me

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -5,6 +5,18 @@ from src.schemas.bookings import BookingAddRequest, BookingAdd
 
 router = APIRouter(prefix="/bookings", tags=["Бронирование"])
 
+@router.get('/')
+async def get_bookings(
+        db: DBDep
+):
+    return await db.bookings.get_all()
+
+@router.get('/me')
+async def get_my_bookings(
+        db: DBDep,
+        user_id: UserIdDep,
+):
+    return await db.bookings.get_filtered(user_id=user_id)
 
 @router.post('/')
 async def create_booking(
@@ -18,4 +30,5 @@ async def create_booking(
     booking = await db.bookings.add(_booking_data)
     await db.commit()
     return {"status": "success", "data": booking}
+
 
