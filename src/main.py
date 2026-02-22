e52fb433cb7c149d1b0740ded0a25d6460d145b2
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-02-22 14:52:10 +0500

    add lifespan, use RedisConnector to get facilities

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
-
+from contextlib import asynccontextmanager
 import sys
 from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
-
+from src.init import redis_connector
 from src.api.hotels import router as hotels_router
 from src.api.auth import router as auth_router
 from src.api.bookings import router as bookings_router
 from src.api.facilities import router as facilities_router
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await redis_connector.connect()
+    yield
+    await redis_connector.close()
+app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router)
 app.include_router(hotels_router)
 app.include_router(bookings_router)
